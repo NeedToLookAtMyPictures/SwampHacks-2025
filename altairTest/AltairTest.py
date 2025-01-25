c30a75f7d668b3511f581e572dcb49a868baee55
NeedToLookAtMyPictures <schmeedainc@gmail.com>
2025-01-25 12:57:46 -0500

    updated AltairTest.py

--- altairTest/AltairTest.py
+++ altairTest/AltairTest.py
@@ -4,8 +4,9 @@ from vega_datasets import data
 cars = data.cars()
 
 # make the chart
-alt.Chart(cars).mark_point().encode(
+chart = alt.Chart(cars).mark_point().encode(
     x='Horsepower',
     y='Miles_per_Gallon',
     color='Origin',
 ).interactive()
+chart.save('chart.html')
